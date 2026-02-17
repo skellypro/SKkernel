d379c3ca1cc6452bbba2c9748a8451037fe3e78a
skellypro <s.kellypro511@gmail.com>
2026-02-16 19:05:33 -0700

    Stubs created for page table entries

---
+++ include/systemk/pagetable/pte.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+
+#include <systemk/pagetable/pte_flags.h>
+
+typedef struct pte_t {
+	union {
+#ifdef x86 || i686 || i586 || i486 || i386
+		uint32_t value;
+		struct {
+		};
+#elifdef x86_64 || x86-64 || amd64 || AMD64
+		uint64_t value;
+		struct {
+		};
+#endif
+	}[[gnu::packed]] [[msvc::__packed]];
+} pte;
