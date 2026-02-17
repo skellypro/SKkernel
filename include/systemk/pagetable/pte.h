c25216b5bc4de72a26b84e6fdfebae470fc7b001
skellypro <s.kellypro511@gmail.com>
2026-02-17 00:14:13 -0700

    Filling in pte_t

--- include/systemk/pagetable/pte.h
+++ include/systemk/pagetable/pte.h
@@ -9,10 +9,32 @@ typedef struct pte_t {
 #ifdef x86 || i686 || i586 || i486 || i386
 		uint32_t value;
 		struct {
+			unsigned present : 1;				// present in memory
+			unsigned rw : 1;					// read/write
+			unsigned user : 1;					// user/superuser
+			unsigned write_through : 1;			// write-through caching
+			unsigned cache_disable : 1;			// cache disable
+			unsigned accessed : 1;				// accessed
+			unsigned dirty : 1;					// dirty bit
+			unsigned pat : 1;					// page attribute table
+			unsigned global : 1;				// global page
+			unsigned available : 3;				// available for system programmer's use
+			unsigned page_frame_number : 20;	// page frame number (shifted right 12 bits)
 		};
 #elifdef x86_64 || amd64 || AMD64
 		uint64_t value;
 		struct {
+			unsigned present : 1;				// present in memory
+			unsigned rw : 1;					// read/write
+			unsigned user : 1;					// user/superuser
+			unsigned write_through : 1;			// write-through caching
+			unsigned cache_disable : 1;			// cache disable
+			unsigned accessed : 1;				// accessed
+			unsigned dirty : 1;					// dirty bit
+			unsigned pat : 1;					// page attribute table
+			unsigned global : 1;				// global page
+			unsigned available : 3;				// available for system programmer's use
+			unsigned page_frame_number : 40;	// page frame number (shifted right 12 bits)
 		};
 #endif
 	}[[gnu::packed]] [[msvc::__packed]];
