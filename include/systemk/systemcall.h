726202606d94d12a6dbec930dd1d9adcd265ab91
skellypro <s.kellypro511@gmail.com>
2026-02-18 14:37:39 -0700

    Fixing conflicts caused by Visual Studio errors.

--- include/systemk/systemcall.h
+++ include/systemk/systemcall.h
@@ -10,7 +10,6 @@
 
 #pragma once
 
-#include <cstdint>
 inline auto [[gnu::fastcall]] [[msvc::__fastcall]]
 systemcall(auto rc, auto rd) {
 	/* 
@@ -92,25 +91,3 @@ systemcall(auto rc, auto rd) {
 #endif
 	//return result;
 }
-=======
-#ifdef x86 || i686 || i586 || i486 || i386
-inline uint32_t [[gnu::fastcall]] [[msvc::__fastcall]]
-systemcall(uint32_t c, uint32_t d) {
-	int32_t [[no_unique_address]]result;
-	asm volatile (
-			"syscall"
-		: "=a"(result) : "c"(c), "d"(d)
-		)
-	return result;
-}
-#elifdef x86_64 || amd64 || AMD64
-inline uint64_t [[gnu::fastcall]] [[msvc::__fastcall]]
-systemcall(uint64_t c, uint64_t d) {
-	int64_t [[no_unique_address]]result;
-	asm volatile (
-			"syscall"
-		: "=a"(result) : "c"(c), "d"(d)
-		)
-	return result;
-}
-#endif
